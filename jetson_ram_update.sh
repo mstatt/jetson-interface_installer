6e3cf99e07da790a74d23128535dfd716cc01e70
Michael Stattelman <mstatt@users.noreply.github.com>
2022-12-30 22:27:21 +0400

    Create jetson_ram_update.sh

---
+++ jetson_ram_update.sh
@@ -0,0 +1,14 @@
+# Disable ZRAM:
+sudo systemctl disable nvzramconfig
+
+# Create 4GB swap file
+sudo fallocate -l 4G /mnt/4GB.swap
+sudo chmod 600 /mnt/4GB.swap
+sudo mkswap /mnt/4GB.swap
+
+# Append the following line to /etc/fstab
+sudo su
+echo "/mnt/4GB.swap swap swap defaults 0 0" >> /etc/fstab
+exit
+
+# Restart the Jetson!
